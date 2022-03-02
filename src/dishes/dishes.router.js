d5adc8b5f1ad46e30f632aa0461b60bd8aae2e9f
Callie Glade <callie.glade@meraki.net>
2022-03-01 19:09:22 -0800

    Add routes for dishes API handlers

--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -1,5 +1,15 @@
 const router = require("express").Router();
+const controller = require("./dishes.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
 
-// TODO: Implement the /dishes routes needed to make the tests pass
+router.route("/:dishId")
+  .get(controller.readDish)     // Responds with the data of the dish matching parameter dishId.
+  .put(controller.updateDish)   // Updates the dish matching parameter dishId and responds with the updated dish data.
+  .all(methodNotAllowed);       // Returns a 405 code for any forbidden or non-existent methods
+
+router.route("/")  
+  .get(controller.listDishes)   // Responds with a list of all existing dish data.
+  .post(controller.createDish)  // Creates a new dish and responds with the newly created dish data.
+  .all(methodNotAllowed);       // Returns a 405 code for any forbidden or non-existent methods
 
 module.exports = router;
