c700c1fcbbf8b5e4e9a31b2213a18dc3e40ad5fe
Callie Glade <callie.glade@meraki.net>
2022-03-01 19:36:32 -0800

    Add routes for orders API handlers

--- src/orders/orders.router.js
+++ src/orders/orders.router.js
@@ -1,5 +1,16 @@
 const router = require("express").Router();
+const controller = require("./orders.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
 
-// TODO: Implement the /orders routes needed to make the tests pass
+router.route("/:orderId")
+  .get(controller.readOrder)        // Responds with the data of the order matching parameter orderId.
+  .put(controller.updateOrder)      // Updates the order matching parameter orderId and responds with the updated order data.
+  .delete(controller.destroyOrder)  // Deletes the order matching parameter orderId and responds with code 204.
+  .all(methodNotAllowed);           // Returns a 405 code for any forbidden or non-existent methods
+
+router.route("/")  
+  .get(controller.listOrders)       // Responds with a list of all existing order data.
+  .post(controller.createOrder)     // Creates a new order and responds with the newly created order data.
+  .all(methodNotAllowed);           // Returns a 405 code for any forbidden or non-existent methods
 
 module.exports = router;
