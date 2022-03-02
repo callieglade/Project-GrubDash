55ec5252d099eec76b44e31362951d40f91973e3
Callie Glade <callie.glade@meraki.net>
2022-03-01 19:22:25 -0800

    Add comments and framework for orders handler middleware

--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -6,4 +6,42 @@ const orders = require(path.resolve("src/data/orders-data"));
 // Use this function to assigh ID's when necessary
 const nextId = require("../utils/nextId");
 
-// TODO: Implement the /orders handlers needed to make the tests pass
+// MIDDLEWARE FUNCTIONS
+
+
+
+// HANDLER FUNCTIONS
+
+// GET /orders => listOrders()
+// Responds with a list of all existing order data.
+function listOrders(req, res) {
+
+}
+
+// POST /orders => createOrder()
+// Creates a new order and responds with the newly created order data.
+function createOrder(req, res) {
+
+}
+
+// GET /orders/:orderId => readOrder()
+// Responds with the data of the order matching parameter orderId.
+function readOrder(req, res) {
+
+}
+
+// PUT /orders/:orderId => updateOrder()
+// Updates the order matching parameter orderId and responds with the updated order data.
+function updateOrder(req, res) {
+  
+}
+
+// DELETE /orders/:orderId => destroyOrder()
+// Deletes the order matching parameter orderId and responds with code 204.
+
+module.exports = {
+  listOrders,
+  createOrder,
+  readOrder,
+  updateOrder,
+}
