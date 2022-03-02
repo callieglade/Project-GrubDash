49e6e670aa829cc07874bcd1f1020f45a094ab17
Callie Glade <callie.glade@meraki.net>
2022-03-01 19:01:52 -0800

    Add comments and framework for dishes handler middleware

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -6,4 +6,39 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 // Use this function to assign ID's when necessary
 const nextId = require("../utils/nextId");
 
-// TODO: Implement the /dishes handlers needed to make the tests pass
+// MIDDLEWARE FUNCTIONS
+
+
+
+// HANDLER FUNCTIONS
+
+// GET /dishes => listDishes()
+// Responds with a list of all existing dish data.
+function listDishes(req, res) {
+
+}
+
+// POST /dishes => createDish()
+// Creates a new dish and responds with the newly created dish data.
+function createDish(req, res) {
+
+}
+
+// GET /dishes/:dishId => readDish()
+// Responds with the data of the dish matching parameter dishId.
+function readDish(req, res) {
+
+}
+
+// PUT /dishes/:dishId => updateDish()
+// Updates the dish matching parameter dishId and responds with the updated dish data.
+function updateDish(req, res) {
+  
+}
+
+module.exports = {
+  listDishes,
+  createDish,
+  readDish,
+  updateDish,
+}
